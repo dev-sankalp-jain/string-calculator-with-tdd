5aa4cc95fa33d75025d92992a940d9bb1709b3ac
Sankalp Jain <dev.sankalp.jain@gmail.com>
2024-06-03 23:59:40 +0530

    Add failing test for if delimiter is new line

--- string_calculator_spec.rb
+++ string_calculator_spec.rb
@@ -7,20 +7,28 @@ describe StringCalculator do
   subject(:calculator) { described_class.new }
 
   describe '#add' do
-    it 'returns 0 for empty string' do
-      expect(calculator.add('')).to eq(0)
+    context 'with vaild inputs' do
+      it 'returns 0 for empty string' do
+        expect(calculator.add('')).to eq(0)
+      end
+
+      it 'returns the same number if only one number is passed' do
+        expect(calculator.add('1')).to eq(1)
+      end
+
+      it 'returns sum of numbers if two numbers are passed' do
+        expect(calculator.add('1,2')).to eq(3)
+      end
+
+      it 'returns sum of numbers if two or more numbers are passed' do
+        expect(calculator.add('1,2,3')).to eq(6)
+      end
     end
 
-    it 'returns the same number if only one number is passed' do
-      expect(calculator.add('1')).to eq(1)
-    end
-
-    it 'returns sum of numbers if two numbers are passed' do
-      expect(calculator.add('1,2')).to eq(3)
-    end
-
-    it 'returns sum of numbers if two or more numbers are passed' do
-      expect(calculator.add('1,2,3')).to eq(6)
+    context 'with new line as a delimiter' do
+      it 'returns sum of numbers' do
+        expect(calculator.add("1\n2,3")).to eq(6)
+      end
     end
   end
 end
