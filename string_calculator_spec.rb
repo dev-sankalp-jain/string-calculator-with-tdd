a82152f71d01a0743d83f79d64aa3e87e1f5d5b4
Sankalp Jain <dev.sankalp.jain@gmail.com>
2024-06-04 00:22:50 +0530

    Add failing test for custom delimiters

--- string_calculator_spec.rb
+++ string_calculator_spec.rb
@@ -30,5 +30,11 @@ describe StringCalculator do
         expect(calculator.add("1\n2,3")).to eq(6)
       end
     end
+
+    context 'with custom delimiters' do
+      it 'returns sum of numbers' do
+        expect(calculator.add("//;\n1;2")).to eq(3)
+      end
+    end
   end
 end
