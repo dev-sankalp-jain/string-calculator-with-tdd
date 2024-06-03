77e6b25e9d7f868fc6279ed4be849f18c4c9df50
Sankalp Jain <dev.sankalp.jain@gmail.com>
2024-06-03 23:57:47 +0530

    Refactor the code: used reduce to compute sum

--- string_calculator.rb
+++ string_calculator.rb
@@ -4,15 +4,6 @@ class StringCalculator
   def add(string_of_numbers)
     return 0 if string_of_numbers.empty?
 
-    numbers = string_of_numbers.split(',')
-
-    return string_of_numbers.to_i if numbers.size == 1
-
-    sum = 0
-    numbers.each do |number|
-      sum += number.to_i
-    end
-
-    sum
+    string_of_numbers.split(',').reduce(0) { |sum, number| sum + number.to_i }
   end
 end
