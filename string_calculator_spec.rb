0b9f2745add34ba66f128635ce758687c6c199fe
Sankalp Jain <dev.sankalp.jain@gmail.com>
2024-06-03 23:25:03 +0530

    Add failing test for an empty string input

--- string_calculator_spec.rb
+++ string_calculator_spec.rb
@@ -4,4 +4,11 @@ require 'rspec'
 require_relative './string_calculator'
 
 describe StringCalculator do
+  subject(:calculator) { described_class.new }
+
+  describe '#add' do
+    it 'returns 0 for empty string' do
+      expect(calculator.add('')).to eq(0)
+    end
+  end
 end
