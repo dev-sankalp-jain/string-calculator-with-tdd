9423d67c3f83d90c783cbe3bed9967bd6592c5db
Sankalp Jain <dev.sankalp.jain@gmail.com>
2024-06-04 00:35:30 +0530

    Implemented functionality to handle custom delimiter

--- string_calculator.rb
+++ string_calculator.rb
@@ -4,6 +4,12 @@ class StringCalculator
   def add(string_of_numbers)
     return 0 if string_of_numbers.empty?
 
-    string_of_numbers.split(/[,\n]/).reduce(0) { |sum, number| sum + number.to_i }
+    if string_of_numbers.start_with?('//')
+      delimiter, string_of_numbers = string_of_numbers.split("\n", 2)
+      delimiter = delimiter[2..]
+      string_of_numbers.split(delimiter).reduce(0) { |sum, number| sum + number.to_i }
+    else
+      string_of_numbers.split(/[,\n]/).reduce(0) { |sum, number| sum + number.to_i }
+    end
   end
 end
