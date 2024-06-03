0f5efe82d71dec81b389e88c4cb039f7343a979f
Sankalp Jain <dev.sankalp.jain@gmail.com>
2024-06-04 01:31:16 +0530

    Implemented functionality to handle negative numbers

--- string_calculator.rb
+++ string_calculator.rb
@@ -1,11 +1,22 @@
 # frozen_string_literal: true
 
 class StringCalculator
+  class NegativeNumberError < StandardError
+    def initialize(numbers)
+      super("negative numbers not allowed: #{numbers.join(', ')}")
+    end
+  end
+
   def add(string_of_numbers)
     return 0 if string_of_numbers.empty?
 
     delimiter, numbers = extract_delimiter_and_numbers(string_of_numbers)
-    numbers.split(delimiter).reduce(0) { |sum, number| sum + number.to_i }
+    numbers = numbers.split(delimiter)
+
+    negative_numbers = numbers.select { |number| number.to_i.negative? }
+    raise NegativeNumberError, negative_numbers if negative_numbers.any?
+
+    numbers.reduce(0) { |sum, number| sum + number.to_i }
   end
 
   private
