cc778d35cd4bad262ef65f508a772ea72a9f5361
Sankalp Jain <dev.sankalp.jain@gmail.com>
2024-06-03 23:31:01 +0530

    Add failing test for single number

--- string_calculator_spec.rb
+++ string_calculator_spec.rb
@@ -10,5 +10,9 @@ describe StringCalculator do
     it 'returns 0 for empty string' do
       expect(calculator.add('')).to eq(0)
     end
+
+    it 'returns the same number if only one number is passed' do
+      expect(calculator.add('1')).to eq(1)
+    end
   end
 end
