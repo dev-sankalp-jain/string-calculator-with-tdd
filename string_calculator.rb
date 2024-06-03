0c36a063e5b3093a91121b9c0e324dc503464863
Sankalp Jain <dev.sankalp.jain@gmail.com>
2024-06-03 23:47:11 +0530

    Implemented functionality to return the sum of numbers if two are passed

--- string_calculator.rb
+++ string_calculator.rb
@@ -4,6 +4,12 @@ class StringCalculator
   def add(string_of_numbers)
     return 0 if string_of_numbers.empty?
 
-    string_of_numbers.to_i
+    numbers = string_of_numbers.split(',')
+
+    if numbers.size == 1
+      return string_of_numbers.to_i
+    else  
+    numbers[0].to_i + numbers[1].to_i
+    end
   end
 end
